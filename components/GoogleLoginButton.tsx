b95489faa9e2967e6b4ffdc0a2e0a3153b4d60a5
Shravan <shravansinghchaudhary@gmail.com>
2024-02-15 20:32:15 +0530

    feat: :sparkles: create google login button component

--- components/GoogleLoginButton.tsx
+++ components/GoogleLoginButton.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const GoogleLoginButton = () => {
   return (
     <button>
-      <GoogleLogin onSuccess={() => {}} />
+      <GoogleLogin onSuccess={(cred) => {}} />
     </button>
   );
 };
