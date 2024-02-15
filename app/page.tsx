f926989dba013c5dd0a7a372a262abb43fdd959a
Shravan <shravansinghchaudhary@gmail.com>
2024-02-15 20:32:52 +0530

    feat: :sparkles: create landing page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import GoogleLoginButton from "@/components/GoogleLoginButton";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Sidebar from "@/components/Sidebar";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className={"text-5xl font-bold"}>Hello World</h1>
-    </div>
+    <MaxWidthWrapper>
+      <div className="mx-auto grid h-screen w-full grid-cols-12">
+        <div className="col-span-3 pl-10 pt-2">
+          <Sidebar />
+        </div>
+        <div className="col-span-6 border-x-[0.5px] border-white/50"></div>
+        <div className="col-span-3">
+          <GoogleLoginButton />
+        </div>
+      </div>
+    </MaxWidthWrapper>
   );
 }
